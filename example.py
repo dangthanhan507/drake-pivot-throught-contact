8a0ad791bf1a7d23ee483ed0f265b854690a4d86
andang <dangthanhan507@gmail.com>
2024-05-11 15:54:14 -0400

    Adding punyo example as a hello world.

---
+++ example.py
@@ -0,0 +1,52 @@
+'''
+    Load bubble.sdf with drake and simulate it with pydrake
+'''
+from pydrake.geometry import StartMeshcat
+from pydrake.multibody.plant import MultibodyPlant, MultibodyPlantConfig, AddMultibodyPlant
+from pydrake.systems.framework import DiagramBuilder
+from pydrake.systems.analysis import Simulator
+from pydrake.math import RigidTransform, RotationMatrix, RollPitchYaw
+from pydrake.visualization import AddDefaultVisualization
+from manipulation.scenarios import AddMultibodyTriad
+from pydrake.multibody.parsing import Parser
+from pydrake.all import Quaternion
+import numpy as np
+
+if __name__ == '__main__':
+    config = MultibodyPlantConfig()
+    config.time_step = 1e-3
+    config.penetration_allowance = 1e-3
+    config.contact_model = "hydroelastic_with_fallback"
+    config.contact_surface_representation = "polygon"
+    config.discrete_contact_approximation = "tamsi"
+    
+    meshcat = StartMeshcat()
+    builder = DiagramBuilder()
+    plant, scene_graph = AddMultibodyPlant(config, builder)
+    plant: MultibodyPlant = plant
+    
+    parser = Parser(plant, scene_graph)
+    
+    
+    # model = parser.AddModels("bubble.sdf")[0]
+    model = parser.AddModelsFromUrl("package://drake_models/wsg_50_description/sdf/schunk_wsg_50_hydro_bubble.sdf")[0]
+    print(model)
+    plant.Finalize()
+    
+    AddDefaultVisualization(builder, meshcat)
+    
+    diagram = builder.Build()
+    
+    context = diagram.CreateDefaultContext()
+    plant_context = plant.GetMyMutableContextFromRoot(context)
+    
+    meshcat.StartRecording()
+    diagram.ForcedPublish(context)
+    context.SetTime(0.0)
+    
+    plant.SetFreeBodyPose(plant_context, plant.GetBodyByName("gripper"), RigidTransform(RotationMatrix.MakeZRotation(np.pi/2), [0, 0, 0.5]))
+    
+    diagram.ForcedPublish(context)
+    meshcat.StopRecording()
+    meshcat.PublishRecording()
+    input()
